14c2251671b0546527de54127aa80460e8aaba0c
awesome-edition-6075 <zachpardee@gmail.com>
2019-03-01 10:44:21 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -19,11 +19,11 @@ def remove_element_from_end_of_array(array)
 end
 
 def remove_element_from_start_of_array(array)
-  array.
+  array.shift
 end
 
 def retrieve_element_from_index(array, index_number)
-  
+  array(index_number)
 end
 
 def retrieve_first_element_from_array(array)
